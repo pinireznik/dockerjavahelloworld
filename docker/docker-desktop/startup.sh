bde68ab7111dc7925bea2fb2d1777c966e93e380
Pini Reznik <p.reznik@uglyduckling.nl>
2014-03-31 17:23:49 +0200

    some changes

--- docker/docker-desktop/startup.sh
+++ docker/docker-desktop/startup.sh
@@ -6,7 +6,8 @@ mkdir /var/run/sshd
 # Add docker user and generate a random password with 12 characters that includes at least one capital letter and number.
 DOCKER_PASSWORD=`pwgen -c -n -1 12`
 echo User: docker Password: $DOCKER_PASSWORD
-DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
+# DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
+DOCKER_ENCRYPYTED_PASSWORD="docker"
 useradd -m -d /home/docker -p $DOCKER_ENCRYPYTED_PASSWORD docker
 sed -Ei 's/adm:x:4:/docker:x:4:docker/' /etc/group
 adduser docker sudo
